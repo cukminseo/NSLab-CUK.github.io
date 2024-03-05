e14c5c73a8f4e154feb106e65fbeeefebaeb3650
NS Lab@CUK <100113405+NSLab-CUK@users.noreply.github.com>
2024-03-05 14:46:02 +0900

    Update particles.js

--- js/vendors/particles.js
+++ js/vendors/particles.js
@@ -28,13 +28,13 @@ var pJS = function(tag_id, params) {
         }
       },
       color: {
-        value: '#fff'
+        value: '#988b8b'
       },
       shape: {
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#ff0000'
+          color: '#000000'
         },
         polygon: {
           nb_sides: 5
@@ -68,7 +68,7 @@ var pJS = function(tag_id, params) {
       line_linked: {
         enable: true,
         distance: 100,
-        color: '#fff',
+        color: '#988b8b',
         opacity: 1,
         width: 1
       },
